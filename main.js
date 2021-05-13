e200091f2696e5d33b3f794a6ab7d5fd9ff7ae53
RishitM <72683716+RishitM@users.noreply.github.com>
2021-05-13 19:21:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var lft_wristx
+var lft_wristy
+var rgt_wristx
+var rgt_wristy
 var song=""
 function preload(){
 song=loadSound("music.mp3");
@@ -7,6 +11,9 @@ function setup(){
     canvas.position(350,200);
     video=createCapture(VIDEO)
     video.hide()
+    console.log(ml5.version)
+    posenet= ml5.poseNet(video,modeloaded)
+    posenet.on("pose",gotPoses)
 }
 function draw(){
     image(video,0,0,700,500)
@@ -19,4 +26,21 @@ function play_music(){
 }
 function stop(){
     song.stop()
+}
+function modeloaded(){
+    console.log("posenet is working")
+}
+function gotPoses(results){
+if (results.length>0){
+    console.log(results)
+lft_wristx=results[0].pose.leftWrist.x
+lft_wristy=results[0].pose.leftWrist.y
+rgt_wristx=results[0].pose.rightWrist.x
+rgt_wristy=results[0].pose.rightWrist.y
+console.log(lft_wristx);
+console.log(lft_wristy);
+console.log(rgt_wristx);
+console.log(rgt_wristy);
+
+}
 }
